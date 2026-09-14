eeb61ca860a3e0f1f1955fc068d127f2967b0af1
marcdiedricks <134682072+marcdiedricks@users.noreply.github.com>
2026-09-14 16:24:03 +0200

    Bump cache for Rules and Signs R2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="mzansi-learner-driver-rules-signs-r2";
+const CACHE="mzansi-learner-driver-rules-signs-r3";
 const ASSETS=["./","./index.html","./style.css","./i18n.js","./app.js","./manifest.json","./icons/mzansi-learner.svg","./icons/icon-192.png","./icons/icon-512.png","./data/questions/rules.json","./data/questions/signs.json","./data/questions/controls.json","./data/questions/motorcycle.json"];
 
 self.addEventListener("install",event=>{
