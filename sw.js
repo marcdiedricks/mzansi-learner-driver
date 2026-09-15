afc3fbf209b69a104c9bddbd1188ca045c58685b
marcdiedricks <134682072+marcdiedricks@users.noreply.github.com>
2026-09-15 12:04:36 +0200

    Cache held NaTIS knowledge remediation pack

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="mzansi-learner-driver-content-completion-qa1";
-const ASSETS=["./","./index.html","./style.css","./i18n.js","./app.js","./manifest.json","./icons/mzansi-learner.svg","./icons/icon-192.png","./icons/icon-512.png","./data/questions/rules.json","./data/questions/signs.json","./data/questions/controls.json","./data/questions/motorcycle.json","./data/questions/completion-rules.json","./data/questions/completion-signs.json","./data/questions/completion-controls.json"];
+const CACHE="mzansi-learner-driver-natis-audit-r1";
+const ASSETS=["./","./index.html","./style.css","./i18n.js","./app.js","./manifest.json","./icons/mzansi-learner.svg","./icons/icon-192.png","./icons/icon-512.png","./data/questions/rules.json","./data/questions/signs.json","./data/questions/controls.json","./data/questions/motorcycle.json","./data/questions/completion-rules.json","./data/questions/completion-signs.json","./data/questions/completion-controls.json","./data/knowledge/rules-remediation-r1.json"];
 
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
